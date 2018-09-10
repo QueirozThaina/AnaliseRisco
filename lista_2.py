64618b9cf0ac3294f182347aee412312776f1cd2
Thaina Queiroz <thaii.queiroz95@gmail.com>
2018-09-10 17:18:10 -0300

    Alterando q1

--- lista_2.py
+++ lista_2.py
@@ -4,36 +4,12 @@
 
 #questao 1
 def media(a,b):
-    """
-    >>> media(2,2)
-    2.0
-    >>> media(2,5)
-    3.5
-    >>> media(8,0)
-    4.0
-    """
     return (a+b)/2.0
 
 def med_4(x1,x2,x3,x4):
-    """
-    >>> med_4(1,2,3,4)
-    2.5
-    >>> med_4(3,7,2,0)
-    3.0
-    >>> med_4(10,50,5,1)
-    16.5
-    """
     return media(media(x1,x2),media(x3,x4))
 
 def bombons(dinheiro, preco):
-    """
-    >>> bombons(10,2.5)
-    (4.0, 0.0)
-    >>> bombons(3,1.20)
-    (2.0, 0.6000000000000001)
-    >>> bombons(50,0.30)
-    (166.0, 0.20000000000000184)
-    """
     return dinheiro//preco, dinheiro%preco
 
 import math
@@ -80,6 +56,3 @@ def bolo(a,b,c):
     leite = c/5
     return min(farinha, ovos, leite)
 
-
-import doctest
-doctest.testmod()
