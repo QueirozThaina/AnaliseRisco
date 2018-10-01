76f8b40ccbec16f7ad7d4f84caafae5bab0416c5
Thaina Queiroz <thaii.queiroz95@gmail.com>
2018-10-01 20:48:16 -0300

    Versão sem o final da questão2

--- Lista05.R
+++ Lista05.R
@@ -6,13 +6,8 @@ pred <- list(NULL,1,1,1,c(1,2),c(1,4),c(1,2), c(1,2,3,5),c(1,2,3,5,7,8))
 est <- numeric(9)
 eft <- numeric(9)
 
-
-#Aproximação empirica
-#vamos supor que a duração de cada atividade sera dada pela média das triangulares
-#Aproximação Empirica
-d <- c(0,2,6,4,3,5,4,2,0)
-
-cmpf <- function(s,est,eft){
+#Tempo mais cedo de incio
+cmpf <- function(s,est,eft,d){
   
   eft[s] <- est[s] + d[s]
   
@@ -21,17 +16,17 @@ cmpf <- function(s,est,eft){
     if(est[i] < eft[s]){
       est[i] <- eft[s]
     }
-    est <- cmpf(i, est,eft)
+    est <- cmpf(i, est,eft,d)
   }
   est
 }
 
 #Passo de volta
 #lft = est no passo de volta do ultimo nó 
-lft <- cmpf(1,est,eft)
+lft <- cmpf(1,est,eft,d)
 lst <- numeric(9) #vetor que vai ser preenchido
 
-cpmb <- function(s = 9,lft,lst){
+cpmb <- function(s = 9,lft,lst,d){
   
   print("Iniciando chamada ...")
   
@@ -54,29 +49,77 @@ cpmb <- function(s = 9,lft,lst){
   lft
 }
 
+###################################### QUESTÃO 1 #######################################
+mediad <- function(a,b,c){
+  media <- (a+b+c)/3
+  media
+}
+
+varianciad <- function(a,b,c){
+  variancia <- (a^2 + b^2 + c^2 - a*b - a*c  - b*c)/18
+  variancia
+}
+
 ########################################################################################
-q1 <- function(){
-  #Aproximação empirica:
-  #Vamos considerar que a duração de cada atividade é a média da triangular
-  #relacionada aquela atividade
-  d <- c(0,2,6,4,3,5,4,2,0)
+#Questao 1 - A
+#Obter uma aproximação empirica da duração do projeto 
+
+#Vamos considerar que a duração de cada atividade é a média da triangular
+#relacionada aquela atividade
+medVar <- function(){
+  A <- c(1,4,2)
+  B <- c(5,7,6)
+  C <- c(2,5,4)
+  D <- c(1,4,3)
+  E <- c(4,7,5)
+  f <- c(3,5,4)
+  G <- c(1,3,2)
   
+  tri <- cbind(0,A,B,C,D,E,f,G,0)
+  vecMedia <- numeric(9)
+  vecVar <- numeric(9)
+  for (i in 1:9){
+    vecMedia[i] <- round(mediad(tri[1,i],tri[2,i],tri[3,i]))
+    vecVar[i] <- varianciad(tri[1,i],tri[2,i],tri[3,i])
+  }
+  x <- cbind(vecMedia, vecVar)
   
-  tInicio <- cmpf(1,est,eft)
-  tFinal <- cpmb(9,lft,lst)
-  #tFinal <- c(0,4,3,0,6,4,7,9,11)
+  x
+}
+
+
+q1A <- function(){
+
+  vec <- medVar()
+  d <- vec[,1]
+  varD <- vec[,2]
+  print(d)
+  
+  tInicio <- cmpf(1,est,eft,d)
+  #tFinal <- cpmb(9,lft,lst)
+  tFinal <- c(0,4,3,0,6,4,7,9,11)
   
   folga <- tFinal - tInicio
   print(folga)
   
   #Menor tempo de duração do projeto é dado pelo tempo de duração do caminho critico
-  tempoTotal <- sum(d[folga==0])
+  media <- sum(d[folga==0])
+  variancia <- sum(varD[folga==0])
   
-  tempoTotal
-
+  tcl <- rnorm(3000, media, variancia)
+  
+  hist(tcl)
+  
+  tcl
+  
 }
 
+
 ########################################################################################
+#Questao 1 - B
+#Obter uma estimativa das probabilidades das atividades que pertecem ao caminho critico
+
+
 library(triangle)
 #Retorna um vetor aleatório de duração das atividades
 duracao <- function(){
@@ -89,16 +132,16 @@ duracao <- function(){
   f <- rtriangle(1,3,5,4)
   G <- rtriangle(1,1,3,2)
   
-  d <- c(A,B,C,D,E,f,G)
+  d <- c(0,A,B,C,D,E,f,G,0)
   
-  d
+  round(d)
 }
 
 #Calcula o tempo de duração de um projeto
 #A partir de um vetor de duração de atividades gerado aleatóriamente
 tempoAtividade <- function(){
   
-  d <- duracao
+  d <- duracao()
   
   tInicio <- cmpf(1,est,eft)
   tFinal <- cpmb(9,lft,lst)
@@ -115,37 +158,116 @@ tempoAtividade <- function(){
 }
 
 #Faz uma estimativa do tempo de duração do projeto utilizando Monte Carlo
-q2 <- function(){
+q1B <- function(){
   tempoMC <- replicate(3000, tempoAtividade())
   
   hist(tempoMC)
+  
+  tempoMC
 }
 
 ########################################################################################
-PERT <- function(){
-  #Aproximação empirica:
-  #Vamos considerar que a duração de cada atividade é a média da triangular
-  #relacionada aquela atividade
-  d <- c(0,2,6,4,3,5,4,2,0)
+#Questao 1 - C
+#Comparar os resultados obtidos por Monte Carlo com os resultados obtido por PERK
+q1C <- function(){
+  perk <- q1A()
+  MC <- q1B()
   
-  var <- #terminar 
-    
-    tInicio <- cmpf(1,est,eft)
-  tFinal <- cpmb(9,lft,lst)
-  #tFinal <- c(0,4,3,0,6,4,7,9,11)
+  qqnorm(perk, col="blue")
+  lines(qqmorm(MC), col="red")
+  
+}
+
+########################################################################################
+#Questao 1 - D
+
+q1D <- function(){
+  
+  matAtividades <- matrix(0,nrow=1000,ncol=7)
+  mat <- medVar()[2:8,]
+  for (i in 1:7){
+    matAtividades[,i] <- rnorm(1000, mat[i,1], mat[i,2])
+  }
+  #histograma Atividade A
+  hist(matAtividades[,1], main = "Atividade A", xlab = "Duração", ylab = "Frequência",
+       col="lightblue")
+  #histograma Atividade B
+  hist(matAtividades[,2], main = "Atividade B", xlab = "Duração", ylab = "Frequência",
+       col="lightpink")
+  #histograma Atividade C
+  hist(matAtividades[,3], main = "Atividade C", xlab = "Duração", ylab = "Frequência",
+       col="orange")
+  #histograma Atividade D
+  hist(matAtividades[,4], main = "Atividade D", xlab = "Duração", ylab = "Frequência",
+       col="lightgreen")
+  #histograma Atividade E
+  hist(matAtividades[,5], main = "Atividade E", xlab = "Duração", ylab = "Frequência",
+       col = "turquoise")
+  #histograma Atividade F
+  hist(matAtividades[,6], main = "Atividade F", xlab = "Duração", ylab = "Frequência",
+       col="yellow1")
+  #histograma Atividade G
+  hist(matAtividades[,7], main = "Atividade G", xlab = "Duração", ylab = "Frequência",
+       col="grey")
+}
+
+###################################### QUESTÃO 2 #######################################
+
+########################################################################################
+#Questao 2 - A 
+succ <- list(c(2,3),4,6,5,6,c(7,8),c(9,10),9,11,11,NULL)
+pred <- #fazer
+  est2 <- numeric(11)
+eft2 <- numeric(11)
+
+medVar2 <- function(){
+  A <- c(2,18,4)
+  B <- c(5,19,9)
+  C <- c(4,28,10)
+  D <- c(8,36,13)
+  E <- c(44,100,60)
+  f <- c(30,74,40)
+  G <- c(9,43,10)
+  H <- c(24,48,30)
+  I <- c(28,96,29)
+  J <- c(10,12,10)
+  
+  tri <- cbind(0,A,B,C,D,E,f,G,H,I,J,0)
+  
+  vecMedia <- numeric(12)
+  vecVar <- numeric(12)
+  
+  for (i in 1:12){
+    vecMedia[i] <- round(mediad(tri[1,i],tri[2,i],tri[3,i]))
+    vecVar[i] <- varianciad(tri[1,i],tri[2,i],tri[3,i])
+  }
+  
+  cbind(vecMedia, vecVar)
+}
+
+
+RiscoPrazo <- function(){
+  d <- medVar2()[,1]
+  varD <- medVar2()[,2]
+  tInicio <- cmpf(1,est2,eft2,d)
+  #tFinal <- cpmb(11,lft2,lst2)
+  tFinal <- c(0,0,30,8,22,41,109,65,174,157,208)
   
   folga <- tFinal - tInicio
   print(folga)
   
   #Menor tempo de duração do projeto é dado pelo tempo de duração do caminho critico
-  tempoTotal <- sum(d[folga==0])
+  media <- sum(d[folga==0])
+  variancia <- sum(varD[folga==0])
+  print(media)
+  print(variancia)
   
-  tempoTotal
+  tcl <- rnorm(3000, media, variancia)
   
-  medTemp <- sum(d[folga==0])
-  varTemp <- sum(var[folga==0])
-  tempoTotal <- rnorm(3000, medTemp, varTemp)
-  hist(tempoTotal)
+  hist(tcl)
+  
+  tcl
 }
 
 
+  
